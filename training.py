54c28fae9f6c8c29d3967c03caffb3f4a648d4e1
vscorza0093 <vinicius.scorza@hotmail.com>
2023-11-28 11:01:30 -0300

    Updating studies

---
+++ training.py
@@ -0,0 +1,8 @@
+n = 8
+
+lista_de_coordenadas = [[a, b, c, d, e, f, g, h]  for a in range(2) for b in range(2) for c in range(2) for d in range(2) for e in range(2) for f in range(2) for g in range(2) for h in range(2)]
+
+
+print(f"Lista de coordenadas: {type(lista_de_coordenadas)}")
+print("----------")
+print(lista_de_coordenadas)
